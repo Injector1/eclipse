1a3516e50444379d5a6e9caa032b312341646853
Tolchenov Andrey <jon.ivan2014@yandex.ru>
2022-05-31 02:35:58 +0500

    feat: bullets explosions
    * raw version of bullets explosions

--- Assets/Scripts/Components/Weapons/PlasmaGun/Bullet.cs
+++ Assets/Scripts/Components/Weapons/PlasmaGun/Bullet.cs
@@ -1,22 +1,29 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private int LifeTime;
     [SerializeField] private float Damage;
+    private Animator _animator;
 
     private void Start()
     {
+        _animator = GetComponent<Animator>();
         ActionPlanner.PostponeAnAction(() => Destroy(gameObject), LifeTime);
     }
     
-    private void OnTriggerStay2D(Collider2D other)
+    private async void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<Collider2D>().isTrigger)
             return;
         var otherHealth = other.gameObject.GetComponent<Health>();
-        if (!(otherHealth is null))
+        if (otherHealth is not null && gameObject.GetComponent<Rigidbody2D>().velocity.magnitude > 1e-9)
             otherHealth.OnHealthChange?.Invoke(-Damage);
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        
+        _animator.SetBool("isDead", true);
+        await Task.Delay(500);
         Destroy(gameObject);
     }
 }
