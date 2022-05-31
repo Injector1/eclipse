cf3ec6577235c859efd109e13c5e7ff14f44e8e2
Tolchenov Andrey <jon.ivan2014@yandex.ru>
2022-05-31 21:04:17 +0500

    feat: new pointer
    * added pointer to nearest enemy

--- Assets/Scripts/Utilities/TargetPointer.cs
+++ Assets/Scripts/Utilities/TargetPointer.cs
@@ -1,65 +1,93 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Utilities
 {
 	public class TargetPointer : MonoBehaviour
 	{
-		[SerializeField] private Transform[] targets;
-		[SerializeField] private RectTransform pointer;
+		[SerializeField] private RectTransform stationPointer;
+		[SerializeField] private RectTransform enemyPointer;
 
+		private GameObserver _observer;
 		private Vector3 _startPointerSize;
 		private Camera _mainCamera;
 		private float _interfaceScale;
 		private int _size;
-	
+
+		private Dictionary<RectTransform, GameObject[]> _targets;
+
 		private void Awake()
 		{
-			_startPointerSize = pointer.sizeDelta;
+			_startPointerSize = stationPointer.sizeDelta;
 			_mainCamera = Camera.main;
 			_interfaceScale = 300;
 			_size = 500;
+
+			_observer = GameObject.Find("Utilities").GetComponent<GameObserver>();
+
+			_targets = new Dictionary<RectTransform, GameObject[]>
+			{
+				{stationPointer, GameObject.FindGameObjectsWithTag("Station")},
+				{enemyPointer, GameObject.FindGameObjectsWithTag("Enemy")}
+			};
+		}
+
+		private float GetDistance(Vector3 t)
+		{
+			var p = _observer.Player.transform.position;
+			return (p.x - t.x) * (p.x - t.x) + (p.y - t.y) * (p.y - t.y);
+		}
+		
+		private GameObject GetNearestTarget(GameObject[] targets)
+		{
+			var distance = targets.Min(t => GetDistance(t.transform.position));
+			return targets.FirstOrDefault(t => Math.Abs(GetDistance(t.transform.position) - distance) < 1);
 		}
 
 		private void LateUpdate()
 		{
-			var target = targets.FirstOrDefault(target => target.gameObject.activeSelf);
-			if (target is null) return;
-			
-			var realPos = _mainCamera.WorldToScreenPoint(target.position);
-			var rect = new Rect(-_size, -_size, Screen.width + _size * 2, Screen.height + _size * 2);
-
-			var outPos = realPos;
-			float direction = 1;
-
-			pointer.gameObject.SetActive(true);
-
-			if (rect.Contains(realPos)) pointer.gameObject.SetActive(false);
-			
-			if (IsBehind(target.position))
+			foreach (var pointer in _targets.Keys)
 			{
-				realPos = -realPos;
-				outPos = new Vector3(realPos.x, 0, 0);
+				var target = GetNearestTarget(_targets[pointer]);
+				if (target is null) return;
 			
-				if (_mainCamera.transform.position.y < target.position.y)
+				var realPos = _mainCamera.WorldToScreenPoint(target.transform.position);
+				var rect = new Rect(-_size, -_size, Screen.width + _size * 2, Screen.height + _size * 2);
+
+				var outPos = realPos;
+				float direction = 1;
+
+				pointer.gameObject.SetActive(true);
+
+				if (rect.Contains(realPos)) pointer.gameObject.SetActive(false);
+			
+				if (IsBehind(target.transform.position))
 				{
-					direction = -1;
-					outPos.y = Screen.height;		
-				}
-			}
-
-			var offset = pointer.sizeDelta.x;
+					realPos = -realPos;
+					outPos = new Vector3(realPos.x, 0, 0);
 			
-			outPos.x = Mathf.Clamp(outPos.x, offset, Screen.width - offset);
-			outPos.y = Mathf.Clamp(outPos.y, offset, Screen.height - offset);
+					if (_mainCamera.transform.position.y < target.transform.position.y)
+					{
+						direction = -1;
+						outPos.y = Screen.height;		
+					}
+				}
 
-			RotatePointer(direction * realPos - outPos);
+				var offset = pointer.sizeDelta.x;
+			
+				outPos.x = Mathf.Clamp(outPos.x, offset, Screen.width - offset);
+				outPos.y = Mathf.Clamp(outPos.y, offset, Screen.height - offset);
 
-			pointer.sizeDelta = new Vector2(
-				_startPointerSize.x / 100 * _interfaceScale,
-				_startPointerSize.y / 100 * _interfaceScale
+				RotatePointer(pointer, direction * realPos - outPos);
+
+				pointer.sizeDelta = new Vector2(
+					_startPointerSize.x / 100 * _interfaceScale,
+					_startPointerSize.y / 100 * _interfaceScale
 				);
-			pointer.anchoredPosition = outPos;
+				pointer.anchoredPosition = outPos;
+			}
 		}
 	
 		private bool IsBehind(Vector3 point)
@@ -70,9 +98,9 @@ namespace Utilities
 				point - cameraTransform.position) < 0;
 		}
 	
-		private void RotatePointer(Vector2 direction)
+		private void RotatePointer(RectTransform p, Vector2 direction)
 		{		
-			pointer.rotation = Quaternion.AngleAxis(
+			p.rotation = Quaternion.AngleAxis(
 				Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, Vector3.forward);
 		}
 	}
