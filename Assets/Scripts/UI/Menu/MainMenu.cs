9121c8906a9905a5b8971b5e2b766bacdfbbf9ad
Tolchenov Andrey <jon.ivan2014@yandex.ru>
2022-06-02 21:54:18 +0500

    feat: sandbox scene
    * added sandbox scene

--- Assets/Scripts/UI/Menu/MainMenu.cs
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.MemoryProfiler;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void PlaySandbox()
+    {
+        SceneManager.LoadScene(4); 
+    }
+    
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
